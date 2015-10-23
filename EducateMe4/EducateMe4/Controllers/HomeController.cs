26780a003f042f74afb847fb51107861f55f700f
adrenaline1 <hristo.tsolev@isdc.eu>
2015-10-23 20:27:46 +0300

    maikata si jeis

--- EducateMe4/EducateMe4/Controllers/HomeController.cs
+++ EducateMe4/EducateMe4/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EducateMe4.Models;
 
 namespace EducateMe4.Controllers {
     public class HomeController : Controller {
         public ActionResult Index() {
-            return View();
+            IndexViewModel model = new IndexViewModel();
+
+            return View(model);
         }
 
         public ActionResult About() {
--- EducateMe4/EducateMe4/Models/IdentityModels.cs
+++ EducateMe4/EducateMe4/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace EducateMe4.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("eduMeDB", throwIfV1Schema: false)
         {
         }
 
